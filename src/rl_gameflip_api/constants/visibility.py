d4342896334a2d44e6eeae6cd13fa4ea4a312c5a
Gustavo Pedroso Bernardes <thevicio27@gmail.com>
2022-12-02 10:57:27 -0300

    Código atualizado refeito, e atualizado para a nova versão do rocket_league_utils.

--- src/rl_gameflip_api/constants/visibility.py
+++
@@ -1,4 +0,0 @@
-DRAFT = "draft"
-READY = "ready"
-ON_SALE = "onsale"
-VISIBILITY = [DRAFT, READY, ON_SALE]
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-from rl_gameflip_api.items import Items
+from rocket_league_gameflip_api import RocketLeagueGameflipAPI
 
-items = Items.from_request()
+api = RocketLeagueGameflipAPI()
+api.download_item_image_by_name_and_color("Jäger 619", r"C:\Users\gugu-\Downloads")
