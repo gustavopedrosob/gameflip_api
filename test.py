a5ed2a43a91b778eac1f2936a18c95fc7164e0b7
Gustavo Pedroso Bernardes <gpedrosobernardes@gmail.com>
2022-12-17 20:54:10 -0300

    Agora os data_item são buscados por um ReprItem, assim como era antes.

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 from rocket_league_gameflip_api import RocketLeagueGameflipAPI
+import rocket_league_utils as rl_utils
 
 api = RocketLeagueGameflipAPI()
+jager = rl_utils.ReprItem("Jäger 619", "car", False, "crimson")
+respective_jager = api.get_data_item(jager)
+jager_url = api.gen_icon_url("Jäger 619")
+
+print(jager_url)
+print(respective_jager.get_full_icon_url(respective_jager.get_icon_by_color(jager.color)))
