69113205d25c6bda4632c86254fb0566c284e449
Gustavo Pedroso Bernardes <gpedrosobernardes@gmail.com>
2022-12-17 18:53:35 -0300

    Atualizando a versão e renomeando a função get_item_data para get_data_item.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="rocket-league-gameflip-api",
-    version="0.0.3",
+    version="0.0.4",
     author="gustavopedrosob",
     author_email="thevicio27@gmail.com",
     description="A Python Rocket League Gameflip API",
