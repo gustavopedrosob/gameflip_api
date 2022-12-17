88a485c37681ec0f0197683ec2f5a953d454ca54
Gustavo Pedroso Bernardes <gpedrosobernardes@gmail.com>
2022-12-17 18:57:28 -0300

    Adicionando a classe ColorfulDataItem ao __init__.py e alterando a tipagem do retorno da função get_data_item

--- src/rocket_league_gameflip_api/__init__.py
+++ src/rocket_league_gameflip_api/__init__.py
@@ -1,3 +1,3 @@
-from rocket_league_gameflip_api.main import DataItem, RocketLeagueGameflipAPIException, ItemNotFound, \
-    RocketLeagueGameflipAPI
+from rocket_league_gameflip_api.main import ColorfulDataItem, DataItem, RocketLeagueGameflipAPIException,\
+    ItemNotFound, RocketLeagueGameflipAPI
 from rocket_league_gameflip_api.constants import *
