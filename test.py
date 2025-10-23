1463f28c0c339878fb6078dc2e9035567adc9835
Gustavo Pedroso Bernardes <gpedrosobernardes@gmail.com>
2025-10-23 16:01:06 -0300

    refactor: removed unused variables refactor: listing method renamed to listings feature: added listing method update: using pydantic for type validation on some methods

--- test.py
+++ test.py
@@ -5,18 +5,22 @@ import pytest
 from gameflip_api import GameflipAPI
 
 
-def test_listing():
-    assert GameflipAPI.listing().status_code == 200
+def test_listings_success():
+    assert GameflipAPI.listings().status_code == 200
 
 
-def test_my_profile():
+def test_my_profile_success():
     api = GameflipAPI(os.getenv('key_api'), os.getenv('secret'))
     assert api.profile().status_code == 200
 
 
-def test_any_profile():
+def test_any_profile_success():
     api = GameflipAPI(os.getenv('key_api'), os.getenv('secret'))
     assert api.profile('us-east-1:dc501f75-302c-4419-8ece-57974d688e6f').status_code == 200
+
+
+def test_any_listing_success():
+    assert GameflipAPI.listing("44e45372-b6f1-4400-af78-419c29243d6c").status_code == 200
 
 
 if __name__ == '__main__':
