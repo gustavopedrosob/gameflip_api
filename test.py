f0ecd66acb5c9bdb5db2364cf8b1004a4b129d33
Gustavo Pedroso Bernardes <gpedrosobernardes@gmail.com>
2025-10-23 17:46:55 -0300

    update: added some tests

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import os
 
 import pytest
-
+from pydantic import ValidationError
+from dotenv import load_dotenv
 from gameflip_api import GameflipAPI
+
+
+load_dotenv()
 
 
 def test_listings_success():
@@ -10,13 +14,37 @@ def test_listings_success():
 
 
 def test_my_profile_success():
-    api = GameflipAPI(os.getenv('key_api'), os.getenv('secret'))
+    api = GameflipAPI(os.getenv('KEY_API'), os.getenv('SECRET'))
     assert api.profile().status_code == 200
 
 
 def test_any_profile_success():
-    api = GameflipAPI(os.getenv('key_api'), os.getenv('secret'))
+    api = GameflipAPI(os.getenv('KEY_API'), os.getenv('SECRET'))
     assert api.profile('us-east-1:dc501f75-302c-4419-8ece-57974d688e6f').status_code == 200
+
+
+def test_empty_profile_id_error():
+    with pytest.raises(ValidationError):
+        api = GameflipAPI(os.getenv('KEY_API'), os.getenv('SECRET'))
+        api.profile("")
+
+
+def test_wrong_type_profile_id_error():
+    with pytest.raises(ValidationError):
+        api = GameflipAPI(os.getenv('KEY_API'), os.getenv('SECRET'))
+        api.profile(0)
+
+
+def test_empty_listing_id_error():
+    with pytest.raises(ValidationError):
+        api = GameflipAPI(os.getenv('KEY_API'), os.getenv('SECRET'))
+        api.listing("")
+
+
+def test_wrong_type_listing_id_error():
+    with pytest.raises(ValidationError):
+        api = GameflipAPI(os.getenv('KEY_API'), os.getenv('SECRET'))
+        api.listing(0)
 
 
 def test_any_listing_success():
