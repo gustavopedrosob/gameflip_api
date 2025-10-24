6ae1c8e2d46226f9fd476ef6da1c4f966bb53806
bernardes.gustavo <thevicio27@gmail.com>
2025-10-23 22:25:49 -0300

    feature: added price validation feature: added range validation update: new tests added

--- src/gameflip_api/main.py
+++ src/gameflip_api/main.py
@@ -3,47 +3,12 @@ import typing
 import requests
 from gameflip_api.enums import ShippingPaidBy, Category, Platform, UPC, ListingStatus
 import pyotp
-from pydantic import BaseModel, Field
 
-
-class IdParam(BaseModel):
-    id_: str = Field(..., min_length=1, description="Id is required.")
-
-
-class GameflipAPIParams(BaseModel):
-    api_key: str = Field(..., min_length=1, description="API key is required.")
-    secret: str = Field(..., min_length=1, description="Secret is required.")
-
-
-class PriceRange:
-    def __init__(self, min_price: int, max_price: int):
-        """
-        :param min_price: An int higher than 75
-        :param max_price: An int higher than 75
-        """
-        self.__min_price = min_price
-        self.__max_price = max_price
-
-    def __str__(self):
-        return f"{self.__min_price},{self.__max_price}"
-
-
-class DateRange:
-    def __init__(self, min_date: datetime.datetime, max_date: datetime.datetime):
-        self.__min_date = min_date
-        self.__max_date = max_date
-
-    def __str__(self):
-        return f"{self.__format_date(self.__min_date)},{self.__format_date(self.__max_date)}"
-
-    @staticmethod
-    def __format_date(date: datetime.datetime):
-        return date.isoformat(timespec='milliseconds') + "Z"
+from gameflip_api.params import GameflipAPIParams, IdParam, ListingsParams, Range, PriceRange, DatetimeRange
 
 
 class GameflipAPI:
     __api = 'https://production-gameflip.fingershock.com/api/v1'
-
 
     def __init__(self, api_key: str, secret: str) -> None:
         """
@@ -99,9 +64,9 @@ class GameflipAPI:
             condition: typing.Optional[str] = None,
             condition_min: typing.Optional[str] = None,
             price: typing.Optional[PriceRange] = None,
-            created: typing.Optional[DateRange] = None,
-            updated: typing.Optional[DateRange] = None,
-            expiration: typing.Optional[DateRange] = None,
+            created: typing.Optional[DatetimeRange] = None,
+            updated: typing.Optional[DatetimeRange] = None,
+            expiration: typing.Optional[DatetimeRange] = None,
             seller_online_until: typing.Optional[datetime.datetime] = None,
             tags: typing.Optional[str] = None,
             start: typing.Optional[int] = None
@@ -134,27 +99,12 @@ class GameflipAPI:
         Returns:
             requests.Response: Returns a response object
         """
-        params = {}
-        if term is not None: params["term"] = term
-        if category is not None: params["category"] = category.value
-        if platform is not None: params["platform"] = platform.value
-        if genre is not None: params["genre"] = genre
-        if upc is not None: params["upc"] = upc.value
-        if shipping_paid_by is not None: params["shipping_paid_by"] = shipping_paid_by.value
-        if digital is not None: params["digital"] = digital
-        if status is not None: params["status"] = status.value
-        if owner is not None: params["owner"] = owner
-        if condition is not None: params["condition"] = condition
-        if condition_min is not None: params["condition_min"] = condition_min
-        if price is not None: params["price"] = str(price)
-        if created is not None: params["created"] = str(created)
-        if updated is not None: params["updated"] = str(updated)
-        if expiration is not None: params["created"] = str(expiration)
-        if seller_online_until is not None:
-            params["seller_online_until"] = seller_online_until.isoformat(timespec='milliseconds') + "Z"
-        if tags is not None: params["tags"] = tags
-        if start is not None: params["start"] = start
-        return requests.get(f"{cls.__api}/listing", params)
+        listings_params = ListingsParams(term=term, category=category, platform=platform, genre=genre, upc=upc,
+                                         shipping_paid_by=shipping_paid_by, digital=digital, status=status, owner=owner,
+                                         condition=condition, condition_min=condition_min, price=price, created=created,
+                                         updated=updated, expiration=expiration,
+                                         seller_online_until=seller_online_until, tags=tags, start=start)
+        return requests.get(f"{cls.__api}/listing", listings_params.model_dump(exclude_none=True))
 
     @classmethod
     def listing(cls, id_: str):
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
+import datetime
 import os
 
 import pytest
 from pydantic import ValidationError
 from dotenv import load_dotenv
 from gameflip_api import GameflipAPI
-
+from gameflip_api.params import PriceRange, Range, ListingsParams, DatetimeRange
 
 load_dotenv()
 
@@ -29,10 +30,11 @@ def test_empty_profile_id_error():
         api.profile("")
 
 
-def test_wrong_type_profile_id_error():
+@pytest.mark.parametrize("value", [0, 0.1, [], {}, set()])
+def test_profile_id_type_error(value):
     with pytest.raises(ValidationError):
         api = GameflipAPI(os.getenv('KEY_API'), os.getenv('SECRET'))
-        api.profile(0)
+        api.profile(value)
 
 
 def test_empty_listing_id_error():
@@ -41,15 +43,64 @@ def test_empty_listing_id_error():
         api.listing("")
 
 
-def test_wrong_type_listing_id_error():
+@pytest.mark.parametrize("value", [0, 0.1, [], {}, set()])
+def test_listing_id_type_error(value):
     with pytest.raises(ValidationError):
         api = GameflipAPI(os.getenv('KEY_API'), os.getenv('SECRET'))
-        api.listing(0)
+        api.listing(value)
+
+
+def test_empty_api_key_error():
+    with pytest.raises(ValidationError):
+        api = GameflipAPI("", os.getenv('SECRET'))
+
+
+@pytest.mark.parametrize("value", [0, 0.1, [], {}, set()])
+def test_api_key_type_error(value):
+    with pytest.raises(ValidationError):
+        api = GameflipAPI(value, os.getenv('SECRET'))
+
+
+def test_empty_secret_error():
+    with pytest.raises(ValidationError):
+        api = GameflipAPI(os.getenv("KEY_API"), "")
+
+
+@pytest.mark.parametrize("value", [0, 0.1, [], {}, set()])
+def test_secret_type_error(value):
+    with pytest.raises(ValidationError):
+        api = GameflipAPI(os.getenv("KEY_API"), "")
 
 
 def test_any_listing_success():
     assert GameflipAPI.listing("44e45372-b6f1-4400-af78-419c29243d6c").status_code == 200
 
 
-if __name__ == '__main__':
-    pytest.main([__file__])
+def test_listings_invalid_price_range_error():
+    with pytest.raises(ValidationError):
+        GameflipAPI.listings(price=PriceRange(start=0, end=100))
+
+
+@pytest.mark.parametrize(
+    "start, end",
+    [
+        (100, 10),
+        (datetime.datetime(2025, 1, 1), datetime.datetime(2024, 1, 1))
+    ]
+)
+def test_invalid_range(start, end):
+    with pytest.raises(ValidationError):
+        Range(start=start, end=end)
+
+
+def test_listing_formats():
+    datetime_1 = datetime.datetime(2025, 1, 1)
+    datetime_2 = datetime.datetime(2025, 1, 1, 12)
+    datetime_range = DatetimeRange(start=datetime_1, end=datetime_2)
+    expected_datetime_string = '2025-01-01T00:00:00.000Z,2025-01-01T12:00:00.000Z'
+    listing_params = ListingsParams(price=PriceRange(start=75, end=100), created=datetime_range, updated=datetime_range, expiration=datetime_range, seller_online_until=datetime_range)
+    params = listing_params.model_dump()
+    assert (params['price'] == '75,100' and params['created'] == expected_datetime_string and
+            params['updated'] == expected_datetime_string and params['expiration'] == expected_datetime_string and
+            params['seller_online_until'] == expected_datetime_string)
+
